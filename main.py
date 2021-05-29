ad7b681cc16d8303feda727aa88ea20ea3311c8a
xSeanliux <zhxnliu@yahoo.com.tw>
2021-05-30 02:01:56 +0800

    Added more foolproofing

--- main.py
+++ main.py
@@ -70,10 +70,10 @@ async def gooruh(ctx):
     await ctx.send(embed=embed)
 
 @bot.command(brief = "Shows a zck quotation", description = "Shows a zck quotation from the database")
-async def zisk(ctx, *args):
+async def zisk(ctx, *args): #variable size length
     ## Randomly chooses a quote from ./zck_quotes/zckquotes and displays it
-    def getMsg(idx):
-        if(idx >= len(allquotes)):
+    def getMsg(idx): #Gets the #idx -th quote, formats it, and returns it as a string
+        if(idx >= len(allquotes) || idx < 0):
             return "ZCK#%03d not found." % (idx)
         txt = allquotes[idx].splitlines()
         mxlen = 0
@@ -83,15 +83,16 @@ async def zisk(ctx, *args):
                 continue
             fulltxt = fulltxt + '> 　' + x.strip() + '\n'
             mxlen = max(mxlen, len(x))
-        msg = '> 「\n %s > %s 」——ZCK#%03d' % (fulltxt, '　'*(mxlen + 1), idx)
+        msg = '> 「\n%s> %s 」——ZCK#%03d' % (fulltxt, '　'*(mxlen + 1), idx)
         return msg
 
     if(len(args) == 0):
         idx = random.randint(0, len(allquotes))
-        await getMsg(idx)
+        await ctx.send(getMsg(idx))
     else:
         for _idx in args:
-            await getMsg(_idx)
+            await ctx.send(getMsg(int(_idx)))
+
 
 
 bot.run(token)
