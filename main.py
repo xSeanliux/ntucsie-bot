1c481939b8dc9ad61a38ba0b7587838435a6c2d4
enip2473 <enip2473@gmail.com>
2021-05-30 00:22:21 +0800

    help command update

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 token = os.getenv("DISCORD_TOKEN")
 
 bot = commands.Bot(command_prefix='!')
+bot.remove_command("help")
 
 @bot.event
 async def on_ready():
@@ -16,6 +17,15 @@ async def on_member_join(member):
         "歡迎 {} 加入台大資工新生群!\n".format(member.name) +
         "請看群組中的 announcement 頻道以進行身分認證"
     )
+
+@bot.command()
+async def help(ctx):
+    embed = discord.Embed(Title='Help')
+    embed.add_field(name = "hi", value = "say hi!")
+    embed.add_field(name = "soft", value = "return a photo of soft")
+    embed.add_field(name = "wah", value = "return a photo of ina")
+    embed.add_field(name = "zisk", value = "return a ZCK quotation")
+    await ctx.send(embed=embed)
 
 @bot.command()
 async def hi(ctx):
