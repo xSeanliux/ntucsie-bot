96bf09eec34772f083d0918ac103eb258cbece0e
enip2473 <enip2473@gmail.com>
2021-05-30 11:17:34 +0800

    resolve conflict

--- main.py
+++ main.py
@@ -28,17 +28,6 @@ async def on_member_join(member):
         "歡迎 {} 加入台大資工新生群!\n".format(member.name) +
         "請看群組中的 announcement 頻道以進行身分認證"
     )
-    
-'''
-@bot.command()
-async def help(ctx):
-    embed = discord.Embed(Title='Help')
-    embed.add_field(name = "hi", value = "say hi!")
-    embed.add_field(name = "soft", value = "return a photo of soft")
-    embed.add_field(name = "wah", value = "return a photo of ina")
-    embed.add_field(name = "zisk", value = "return a ZCK quotation")
-    await ctx.send(embed=embed)
-'''
 
 @bot.command(brief = "Says hi!", description = "Says hi!")
 async def hi(ctx):
